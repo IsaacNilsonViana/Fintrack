86b7f77db054e13723c91cdf4ace4c7044a65e0e
Isaac Nilson <isaacnilsonv@gmail.com>
2026-03-12 18:04:31 -0300

    testando e aprendendo sobre services

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,4 @@
+from .user_schema import UserBase, UserCreate, UserResponse, UserUpdate
+from .category_schema import CategoryBase, CategoryCreate, CategoryResponse, CategoryUpdate
+from .account_schema import AccountBase, AccountCreate, AccountResponse, AccountUpdate
+from .transaction_schemas import TransactionBase, TransactionCreate, TransactionResponse, TransactionUpdate
